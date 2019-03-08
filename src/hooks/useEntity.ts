fc53e9fa79e44525b43d769fbee92c32d410f706
Biel Simon Rojas <bielsimon2@gmail.com>
2019-03-08 19:10:29 +0100

    Added useEntity.ts

---
+++ src/hooks/useEntity.ts
@@ -0,0 +1,5 @@
+const useEntity = () => {
+    // Use context / namespace package / reimplement namespaces via a new useNamespace hook
+
+    //Move reference implementations into hooks and use components as wrappers only
+}
